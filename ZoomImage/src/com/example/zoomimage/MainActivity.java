c81b81c29d007b37e12987e0e3e99ca5538c1646
Vu Le <magrabbit@magrabbit-To-be-filled-by-O-E-M.(none)>
2014-02-19 15:06:10 +0700

    change from MRV

--- ZoomImage/src/com/example/zoomimage/MainActivity.java
+++ ZoomImage/src/com/example/zoomimage/MainActivity.java
@@ -21,7 +21,7 @@ import android.widget.ImageView;
  */
 public class MainActivity extends Activity {
 	 /**
-	 * check current animation
+	 * check current animation..haha
 	 */
 	private Animator mCurrentAnimator;
 	    private int mShortAnimationDuration;
